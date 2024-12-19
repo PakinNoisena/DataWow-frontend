9661c61358f509c0d59897bd7c76d9814f25a4b4
GOLF <pakin.n@muze.co.th>
2024-12-19 20:08:04 +0700

    add: createPost repo

--- frontend/src/services/repositories/post.ts
+++ frontend/src/services/repositories/post.ts
@@ -28,5 +28,32 @@ export default () => {
         throw error;
       }
     },
+
+    async createPost(
+      data: { title: string; description: string; communityId: number },
+      userId: string
+    ) {
+      try {
+        const response = await fetch("http://localhost:3000/dataWow/post", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "user-id": userId,
+          },
+          body: JSON.stringify(data),
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const result = await response.json();
+
+        return result;
+      } catch (error) {
+        console.error("Error creating post:", error);
+        throw error;
+      }
+    },
   };
 };
